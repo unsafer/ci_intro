1ea788d447aabfd994f4e5a21719588c03cb813d
Luis <Luis@192.168.76.224>
2014-11-24 16:20:55 -0300

    - se arregla view_login

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -1,123 +1,42 @@
 <!DOCTYPE html>
-
 <html lang="en">
-
-
-    
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/> 
-    <link href="<?= base_url();?>bootstrap/css/bootstrap.css" rel="stylesheet">
-    <script src="<?= base_url();?>bootstrap/js/jquery-1.11.1.min.js"></script>
-    <script src="<?= base_url();?>bootstrap/js/bootstrap.min.js"></script>
-	<title> <?php echo $title; ?> </title>
-
+    <meta charset="utf-8">
+    <link href="<?php echo base_url(); ?>bootstrap/css/global.css" rel="stylesheet" type="text/css">
+       
+    <title> Login Page </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
+    <link href="<?php echo base_url(); ?>assets/css/reset.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/supersized.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-
+ 
 <div id="container">
-	<a href="home">Home</a>
-        <a href="insumos">Insumos</a>
-        <a href="frutas">Frutas</a>
-        <a href="jugos">Jugos</a>
-        <a href='<?php echo base_url() ."site/logout" ?>'>Cerrar Sesion</a>
-	<h2>Accediendo a Base de Datos de Jugos!</h2>
+    <h1><br>Sistema de Administracion!</h1><br><br>
+    
+    
+    
+    <?php echo form_open('site/login_validation'); 
+    
+     echo validation_errors(); 
+            $opts = 'placeholder="Ingrese su Email"';
+                
+            echo form_input( 'email', '', $opts);   
+            echo form_password('password', 'password'); 
+            echo form_submit( 'login_validation', 'Iniciar Sesion' ); 
         
+     echo form_close(); 
+    
+    ?>
+    
 
-    <div class="container" style="padding-top: 1em;">
-      <h1>Tabla de Ingredientes!</h1>
-      <table class="table table-hover">
-          <thead>
-                  <tr>
-                              <th>Nombre del Ingredientes</th>
-                              <th>Precio</th>
-                              <th></th>
-                              
-
-                  </tr>
-          </thead>
-          <tbody>
-           <?php
-                              foreach($results as $row) { ?>
-                  <tr>
-                       <td>Agua</td><td><?php echo $row->agua ?></td><td ><button type="button" class="btn btn-default" title="Add this item" data-id="1234" data-toggle="modal" data-target="#myModal">Modificar</button></td>                               
-                  </tr>
-                  <tr>
-  
-                       <td>Azucar</td><td><?php echo $row->azucar ?></td><td>Moficar</td>  
-                  </tr>
-                  <tr>
-
-                        <td>Leche</td><td><?php echo $row->leche ?></td><td>Moficar</td>  
-                  </tr>
-                                          
-            <?php } ?>                              
-                  
-          </tbody>
-      </table>
-
-      <h1>Tabla de Gastos!</h1>
-      <table class="table table-hover">
-          <thead>
-                  <tr>
-                              <th>Nombre del Gasto</th>
-                              <th>Valor</th>
-                              <th></th>
-                              
-
-                  </tr>
-          </thead>
-          <tbody>
-           <?php
-                              foreach($results as $row) { ?>
-                  <tr>
-                       <td>Gastos Comunes</td><td><?php echo $row->gastoscomunes ?></td><td>Moficar</td>                               
-                  </tr>
-                  <tr>
-  
-                       <td>Mano de Obra</td><td><?php echo $row->manodeobra ?></td><td>Moficar</td>  
-                  </tr>
-
-                                          
-            <?php } ?>                              
-                  
-          </tbody>
-      </table>
-      
-</div>
-        
-</div>
-
+        <script src="assets/js/jquery-1.8.2.min.js"></script>
+        <script src="assets/js/supersized.3.2.7.min.js"></script>
+        <script src="assets/js/supersized-init.js"></script>
+        <script src="assets/js/scripts.js"></script>
 </body>
 </html>
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modificar Agua</h4>
-      </div>
-      <div class="modal-body">
-        <div class="modal-body">
-            <p>Precio:</p>
-            <?php $agua = $row->agua; ?>
-            
-            <form action="<?php echo base_url();?>Site/insert" method="post">
-            <input type = 'text' name='f1' value="<?=$row->agua?>"<br>
-            
-            <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-            <button type='submit' class="btn btn-primary">Guardar Cambios</button>
-            </div>
-            </form>
-            
-        </div>
-      </div>
-      
-        
-    </div>
-  </div>
-</div>
-
-<!--hacer modals para cada modificar-->
