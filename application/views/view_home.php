ed89ff96314661ac938232642dc3d60fab2147c6
Ramirez <Ramirez@Ramirez-PC>
2014-11-26 02:04:11 -0300

    view home

--- application/views/view_home.php
+++ application/views/view_home.php
@@ -13,10 +13,10 @@
 	<title> <?php echo $title; ?> </title>
 
 </head>
-<body>
+<body background="<?= base_url();?>assets\img\backgrounds\4.jpg">
 
 <div id="container">
-        <ul class="nav nav-tabs">
+        <ul class="nav nav-pills">
         <li class="active"> <a href="home">Home</a></li>
         <li><a href="insumos">Insumos</a></li>
         <li><a href="frutas">Frutas</a></li>
@@ -29,7 +29,14 @@
         
         <h2>Pagina de Inicio</h2>
         
+        
+<center>
+<div align ="middle" id="TT_RiJEkk11kA2BfeIAjAxDjjzDjWuATf2lbY1t1Zy5aEz53I3IG"><a href="http://www.tutiempo.net/arica.html">El Tiempo en Arica</a></div>
+<script type="text/javascript" src="http://www.tutiempo.net/widget/eltiempo_RiJEkk11kA2BfeIAjAxDjjzDjWuATf2lbY1t1Zy5aEz53I3IG"></script>
 
+<br>
+
+ <iframe src="https://www.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;height=400&amp;wkst=2&amp;bgcolor=%239999ff&amp;src=fq2moj9qflaab385g935k2f68g%40group.calendar.google.com&amp;color=%23333333&amp;ctz=America%2FSantiago" style=" border-width:0 " width="1000" height="400" frameborder="0" scrolling="no"></iframe> </center>
 
 
                 
